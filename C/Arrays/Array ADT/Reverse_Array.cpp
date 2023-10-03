f43683ea3602791f7b37537871aeae706a2f2619
AlpanaDixit <alpanadixit3@gmail.com>
2023-10-03 18:26:33 +0530

    Array

---
+++ C/Arrays/Array ADT/Reverse_Array.cpp
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+struct Array
+{
+    int A[10];
+    int size;
+    int length;
+};
+
+void Display(struct Array arr)
+{
+    int i;
+    printf("\nElements are\n");
+    for(i=0;i<arr.length;i++)
+        printf("%d ", arr.A[i]);
+}
+
+void Append(struct Array *arr, int x)
+{
+    if (arr->length<arr->size)
+        arr->A[arr->length++]=x;
+}
+
+void Insert(struct Array *arr, int index, int x)
+{
+    int i;
+    if(index>=0 && index <=arr->length)
+    {
+        for(i=arr->length;i>index;i--)
+            arr->A[i]=arr->A[i-1];
+        arr->A[index]=x;
+        arr->length++;
+    }
+}
+
+int Delete(struct Array *arr, int index)
+{
+    int x=0;
+    int i;
+
+    if(index>=0 && index<arr->length)
+    {
+        x=arr->A[index];
+        for(i=index;i<arr->length-1;i++)
+            arr->A[i]=arr->A[i+1];
+        arr->length--;
+        return x;
+    }
+    
+    return 0;
+}
+
+void Reverse(struct Array *arr)
+{
+    int *B;
+    int i,j;
+    B=(int *)malloc(arr->length*sizeof(int));
+    for(i=arr->length-1,j=0;i>=0;i--,j++)
+        B[j]=arr->A[i];
+    for(i=0;i<arr->length;i++)
+        arr->A[i]=B[i];
+}
+
+void Reverse2(struct Array *arr)
+{
+    int i,j;
+    for(i=0,j=arr->length-1;i<j;i++,j--)
+    {
+        
+    }
+}
+
+int main()
+{
+    struct Array arr={{2,3,4,5,6},10,5};
+
+
+    // Append(&arr,10);
+    // Insert(&arr,3,10);
+    // printf("%d\n", Delete(&arr,4));
+    Reverse(&arr);
+    Display(arr);
+
+    return 0;
+
+}
